7c627f18a503780f2372b1d8e6d0eac73fc3b0de
email <niklashallgren03@gmail.com>
2024-04-25 23:04:32 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AUTHORIZATION_TOKEN = os.getenv('AUTHORIZATION_TOKEN')
+
+def fetch_auctions(auth_token):
+    url = "https://eu.api.blizzard.com/data/wow/connected-realm/5828/auctions/2"
+    params = {
+        "namespace": "dynamic-classic1x-eu",
+        "locale": "en_US"
+    }
+    headers = {
+        "Authorization": f"Bearer {auth_token}"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    
+    if response.status_code == 200:
+        return response.json() 
+    else:
+        return f"Failed to fetch data: {response.status_code}"
+
+def find_auctions_by_item_id(auctions_data, item_id):
+    matching_auctions = []
+    for auction in auctions_data.get('auctions', []):
+        if auction['item']['id'] == item_id:
+            matching_auctions.append(auction)
+    return matching_auctions
+
+def sum_bids_for_item(auctions):
+    total_bid = sum(auction['bid'] for auction in auctions)
+    return total_bid
+
+auth_token = AUTHORIZATION_TOKEN
+data = fetch_auctions(auth_token)
+
+if isinstance(data, dict): 
+    item_id = 16204 
+    matching_auctions = find_auctions_by_item_id(data, item_id)
+    if matching_auctions:
+        total_bid_amount = sum_bids_for_item(matching_auctions)
+        print(f"Total bid amount for item ID {item_id} (gold): {total_bid_amount / 10000}")
+    else:
+        print(f"No auctions found for item ID {item_id}.")
+else:
+    print(data)
---
+++ readme.md
@@ -0,0 +1,1 @@
+Find the total cost for a single item. Currently set up for EU crusader strike Alliance. Modify if you want.
