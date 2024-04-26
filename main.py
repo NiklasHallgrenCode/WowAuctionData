353cc486ba6087be344105c44af6d2b38d4dffa4
email <niklashallgren03@gmail.com>
2024-04-26 10:04:55 +0200

    Uncringe dynamic values into variables

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 AUTHORIZATION_TOKEN = os.getenv('AUTHORIZATION_TOKEN')
+ITEM_ID = 16204
+MAX_COST_PER_ITEM = 50000 #Copper
+
 
 def fetch_auctions(auth_token):
     url = "https://eu.api.blizzard.com/data/wow/connected-realm/5828/auctions/2"
@@ -23,10 +26,10 @@ def fetch_auctions(auth_token):
     else:
         return f"Failed to fetch data: {response.status_code}"
 
-def find_auctions_by_item_id(auctions_data, item_id):
+def find_auctions_by_item_id(auctions_data):
     matching_auctions = []
     for auction in auctions_data.get('auctions', []):
-        if auction['item']['id'] == item_id and (auction['buyout'] / auction['quantity'])  < 50000:
+        if auction['item']['id'] == ITEM_ID and (auction['buyout'] / auction['quantity'])  < MAX_COST_PER_ITEM:
         # if auction['item']['id'] == item_id :
             matching_auctions.append(auction)
     matching_auctionz = sorted(matching_auctions, key=lambda x: x['buyout'])
@@ -40,12 +43,12 @@ auth_token = AUTHORIZATION_TOKEN
 data = fetch_auctions(auth_token)
 
 if isinstance(data, dict): 
-    item_id = 16204 
-    matching_auctions = find_auctions_by_item_id(data, item_id)
+    matching_auctions = find_auctions_by_item_id(data)
+
     if matching_auctions:
         total_bid_amount = sum_bids_for_item(matching_auctions)
-        print(f"Total buyout amount for item ID {item_id} (gold): {total_bid_amount / 10000}")
+        print(f"Total buyout amount for item ID {ITEM_ID} (gold): {total_bid_amount / 10000}")
     else:
-        print(f"No auctions found for item ID {item_id}.")
+        print(f"No auctions found for item ID {ITEM_ID}.")
 else:
     print(data)
